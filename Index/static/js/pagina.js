9f9f279561e35b86629ebee8312b8e2a5507b609
victorvalenciab <92226112+victorvalenciab@users.noreply.github.com>
2024-04-15 23:10:31 -0500

    version1

---
+++ Index/static/js/pagina.js
@@ -0,0 +1,6 @@
+let BotonLogin = document.getElementById('Login');
+BotonLogin.addEventListener("click", ()=>
+{
+    BotonLogin.textContent = "Victor";
+    // window.location.href = '../Usuarios/templates/login.html';
+});
--- Index/static/pagina.js
+++
@@ -1,4 +0,0 @@
-
-function irAOtraPagina() {
-    // Reemplaza 'otra-pagina.html' con la URL de la página a la que deseas redirigir
-    window.location.href = './Usuarios/templates/login.html'}
