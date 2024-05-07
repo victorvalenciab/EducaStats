342f5da7f3a0b612868c1348b24dcea218e1475c
victorvalenciab <92226112+victorvalenciab@users.noreply.github.com>
2024-05-07 00:14:27 -0500

    v6

--- Index/static/lib/funciones.js
+++ Index/static/lib/funciones.js
@@ -1,4 +1,3 @@
-
 function Click(url)
 {
 
--- panel_estudiantes/static/lib/tables.js
+++ panel_estudiantes/static/lib/tables.js
@@ -1,7 +1,7 @@
 function toggleTable(grado) {
     var table = document.getElementById(grado);
     if (table.style.display === "none") {
-        table.style.display = "block";
+        table.style.display = "inline-block";
     } else {
         table.style.display = "none";
     }
