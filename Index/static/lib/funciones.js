d93d16242dc41133e216933ad03e13ea3cd86391
victorvalenciab <92226112+victorvalenciab@users.noreply.github.com>
2024-05-26 15:48:39 -0500

    v6

--- Index/static/lib/funciones.js
+++ Index/static/lib/funciones.js
@@ -4,3 +4,31 @@ function Click(url)
     window.location.href = url;
         
 }
+
+// Obtenemos el header
+const header = document.querySelector('header');
+
+// Guardamos la altura del header
+const headerHeight = header.offsetHeight;
+
+// Posición del último scroll
+let lastScrollTop = 0;
+
+// Al hacer scroll
+window.addEventListener('scroll', () => {
+    // Posición actual del scroll
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Si el scroll es hacia abajo y no estamos en la parte superior de la página
+    if (currentScroll > lastScrollTop && currentScroll > headerHeight) {
+        // Ocultamos el header
+        header.classList.add('hidden');
+    } else {
+        // Mostramos el header
+        header.classList.remove('hidden');
+    }
+
+    // Guardamos la posición actual del scroll para comparar en el siguiente evento de scroll
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
